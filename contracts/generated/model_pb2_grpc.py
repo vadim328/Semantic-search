24ddcdb61704c47bd37af61f11ffe8e9fb5de595
Vadim <Vadim32654@gmail.com>
2026-04-07 18:49:34 +0500

    Change path for script

--- contracts/generated/model_pb2_grpc.py
+++ contracts/generated/model_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-from contracts.proto import model_pb2 as contracts_dot_proto_dot_model__pb2
+from contracts.generated import model_pb2 as contracts_dot_proto_dot_model__pb2
 
 GRPC_GENERATED_VERSION = '1.78.0'
 GRPC_VERSION = grpc.__version__
